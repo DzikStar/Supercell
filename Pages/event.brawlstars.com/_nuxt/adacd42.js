90583718b9b489dfaa8e164f40cb9121c9c67190
DzikStar <82441193+DzikStar@users.noreply.github.com>
2022-04-10 20:27:34 +0200

    2022-04-10

---
+++ Pages/event.brawlstars.com/_nuxt/adacd42.js
@@ -0,0 +1,386 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([[22, 24, 25], {
+    1049: function(t, e, a) {
+        var n = a(1072);
+        n.__esModule && (n = n.default),
+        "string" == typeof n && (n = [[t.i, n, ""]]),
+        n.locals && (t.exports = n.locals);
+        (0,
+        a(8).default)("22ad4a68", n, !0, {
+            sourceMap: !1
+        })
+    },
+    1058: function(t, e, a) {
+        var n = a(1091);
+        n.__esModule && (n = n.default),
+        "string" == typeof n && (n = [[t.i, n, ""]]),
+        n.locals && (t.exports = n.locals);
+        (0,
+        a(8).default)("23bfd817", n, !0, {
+            sourceMap: !1
+        })
+    },
+    1068: function(t, e, a) {
+        t.exports = a.p + "img/pin_emo_foam_blue.d0a4293.svg"
+    },
+    1069: function(t, e, a) {
+        t.exports = a.p + "img/pin_emo_glasses.74fa12a.svg"
+    },
+    1070: function(t, e, a) {
+        t.exports = a.p + "img/jessie-crop.45c3e88.png"
+    },
+    1071: function(t, e, a) {
+        "use strict";
+        a(1049)
+    },
+    1072: function(t, e, a) {
+        var n = a(7)
+          , o = a(100)
+          , i = a(310)
+          , r = n((function(t) {
+            return t[1]
+        }
+        ))
+          , d = o(i);
+        r.push([t.i, '.LoginCard[data-v-0bccffe1]{box-shadow:0 2px #040404;border-radius:5px;border:2px solid #040404;justify-content:center;flex-direction:column}.LoginCard[data-v-0bccffe1],.LoginCard__container[data-v-0bccffe1]{position:relative;display:flex;align-items:center;width:100%}.LoginCard__container[data-v-0bccffe1]{flex-direction:row}.LoginCard__background[data-v-0bccffe1],.LoginCard__container[data-v-0bccffe1]{border-top-left-radius:4px;border-top-right-radius:4px;min-height:145px}.LoginCard__background[data-v-0bccffe1]{position:absolute;background-color:#ffd550;overflow:hidden;height:100%;width:100%}.LoginCard__background[data-v-0bccffe1]:after{position:absolute;width:100%;height:100%;top:0;bottom:0;left:0;right:0;content:" ";padding:4px;-webkit-mask-repeat:no-repeat;mask-repeat:no-repeat;-webkit-mask-size:cover;mask-size:cover;-webkit-mask-position:center center;mask-position:center center;background-color:#ffc938;-webkit-mask-image:url(' + d + ");mask-image:url(" + d + ')}.LoginCard h3[data-v-0bccffe1]{text-shadow:-1.5px -1.5px 0 #040404,0 -1.5px 0 #040404,1.5px -1.5px 0 #040404,1.5px 0 0 #040404,1.5px 1.5px 0 #040404,0 1.5px 0 #040404,-1.5px 1.5px 0 #040404,-1.5px 0 0 #040404,0 3px 0 #040404,-1.5px 3px 0 #040404,1.5px 3px 0 #040404;color:#fff;font-weight:400;font-family:"Lilita One",sans-serif;text-transform:none;font-size:18px}.LoginCard h3[data-v-0bccffe1]:lang(jp),.LoginCard h3[data-v-0bccffe1]:lang(kr),.LoginCard h3[data-v-0bccffe1]:lang(pl),.LoginCard h3[data-v-0bccffe1]:lang(ru),.LoginCard h3[data-v-0bccffe1]:lang(tr),.LoginCard h3[data-v-0bccffe1]:lang(zh_sc),.LoginCard h3[data-v-0bccffe1]:lang(zh_tc){font-family:"NotoSans",sans-serif;font-weight:700}.LoginCard h4[data-v-0bccffe1]{color:#040404;font-size:14px;font-family:"Montserrat",sans-serif;text-align:center;margin-top:8px;color:#000}.LoginCard h4[data-v-0bccffe1]:lang(jp),.LoginCard h4[data-v-0bccffe1]:lang(kr),.LoginCard h4[data-v-0bccffe1]:lang(pl),.LoginCard h4[data-v-0bccffe1]:lang(ru),.LoginCard h4[data-v-0bccffe1]:lang(tr),.LoginCard h4[data-v-0bccffe1]:lang(zh_sc),.LoginCard h4[data-v-0bccffe1]:lang(zh_tc){font-family:"NotoSans",sans-serif;font-weight:700}.LoginCard__decorationLeft[data-v-0bccffe1]{position:absolute;top:-28px;left:30%;width:80px;height:60px;transform:rotate(-25deg)}.LoginCard__decorationLeft__image[data-v-0bccffe1]{width:100%;height:auto}.LoginCard__decorationLeft--connected[data-v-0bccffe1]{top:-16px;left:8px;transform:rotate(-55deg)}.LoginCard__decorationRight[data-v-0bccffe1]{position:absolute;width:80px;height:60px;top:-15px;right:-15px;transform:rotate(25deg)}.LoginCard__decorationRight__image[data-v-0bccffe1]{width:100%;height:auto}.LoginCard__text[data-v-0bccffe1]{position:relative;width:60%;padding:32px 16px 16px}.LoginCard__text--connected[data-v-0bccffe1]{display:flex;justify-content:center;align-items:center;flex-direction:column;width:100%;text-align:center;padding:16px}.LoginCard__imageContainer[data-v-0bccffe1]{position:relative;display:flex;width:33%;height:145px}.LoginCard__imageContainer__image[data-v-0bccffe1]{position:absolute;bottom:0;left:0;height:160px;width:auto}.LoginCard__buttonContainer[data-v-0bccffe1]{position:relative;height:64px;width:100%;display:flex;justify-content:center;align-items:center;padding:0 12px;background-color:#9d4540;border-bottom-left-radius:4px;border-bottom-right-radius:4px;border-top:2px solid #040404}.LoginCard__buttonContainer__button[data-v-0bccffe1]{min-width:100%}@media(max-width:23.4275em){.LoginCard h3[data-v-0bccffe1]{text-shadow:-1.5px -1.5px 0 #040404,0 -1.5px 0 #040404,1.5px -1.5px 0 #040404,1.5px 0 0 #040404,1.5px 1.5px 0 #040404,0 1.5px 0 #040404,-1.5px 1.5px 0 #040404,-1.5px 0 0 #040404,0 3px 0 #040404,-1.5px 3px 0 #040404,1.5px 3px 0 #040404;color:#fff;font-weight:400;font-family:"Lilita One",sans-serif;text-transform:none;font-size:18px}.LoginCard h3[data-v-0bccffe1]:lang(jp),.LoginCard h3[data-v-0bccffe1]:lang(kr),.LoginCard h3[data-v-0bccffe1]:lang(pl),.LoginCard h3[data-v-0bccffe1]:lang(ru),.LoginCard h3[data-v-0bccffe1]:lang(tr),.LoginCard h3[data-v-0bccffe1]:lang(zh_sc),.LoginCard h3[data-v-0bccffe1]:lang(zh_tc){font-family:"NotoSans",sans-serif;font-weight:700}.LoginCard__container[data-v-0bccffe1]{min-height:115px}.LoginCard__imageContainer[data-v-0bccffe1]{height:115px}.LoginCard__imageContainer__image[data-v-0bccffe1]{height:145px}.LoginCard__decorationLeft[data-v-0bccffe1]{transform:translate(15px,-10px) scale(.9) rotate(-35deg)}.LoginCard__decorationRight[data-v-0bccffe1]{transform:translate(10px,-12px) scale(.9) rotate(25deg)}}@media only screen and (max-device-width:375px)and (orientation:landscape){.LoginCard h3[data-v-0bccffe1]{text-shadow:-1.5px -1.5px 0 #040404,0 -1.5px 0 #040404,1.5px -1.5px 0 #040404,1.5px 0 0 #040404,1.5px 1.5px 0 #040404,0 1.5px 0 #040404,-1.5px 1.5px 0 #040404,-1.5px 0 0 #040404,0 3px 0 #040404,-1.5px 3px 0 #040404,1.5px 3px 0 #040404;color:#fff;font-weight:400;font-family:"Lilita One",sans-serif;text-transform:none;font-size:18px}.LoginCard h3[data-v-0bccffe1]:lang(jp),.LoginCard h3[data-v-0bccffe1]:lang(kr),.LoginCard h3[data-v-0bccffe1]:lang(pl),.LoginCard h3[data-v-0bccffe1]:lang(ru),.LoginCard h3[data-v-0bccffe1]:lang(tr),.LoginCard h3[data-v-0bccffe1]:lang(zh_sc),.LoginCard h3[data-v-0bccffe1]:lang(zh_tc){font-family:"NotoSans",sans-serif;font-weight:700}.LoginCard__container[data-v-0bccffe1]{min-height:115px}.LoginCard__imageContainer[data-v-0bccffe1]{height:115px}.LoginCard__imageContainer__image[data-v-0bccffe1]{height:145px}.LoginCard__decorationLeft[data-v-0bccffe1]{transform:translate(15px,-10px) scale(.9) rotate(-35deg)}.LoginCard__decorationRight[data-v-0bccffe1]{transform:translate(10px,-12px) scale(.9) rotate(25deg)}}', ""]),
+        r.locals = {},
+        t.exports = r
+    },
+    1074: function(t, e, a) {
+        "use strict";
+        a.r(e);
+        var n = a(0)
+          , o = a(2)
+          , i = a(1087)
+          , r = n.default.extend({
+            name: "FeedLogin",
+            components: {
+                LogInCard: i.default
+            },
+            props: {
+                loggedIn: Boolean,
+                isFullscreen: Boolean
+            },
+            data: function() {
+                return {}
+            },
+            computed: {},
+            mounted: function() {},
+            methods: {
+                dismiss: function() {
+                    this.isFullscreen && (this.loggedIn || this.$emit("on-dismiss-login"))
+                },
+                onEnter: function(t) {},
+                onLeave: function(t) {
+                    o.a.to(t, {
+                        duration: .4,
+                        opacity: 0,
+                        ease: "Power2.easeInOut"
+                    })
+                }
+            }
+        })
+          , d = (a(1090),
+        a(1))
+          , s = Object(d.a)(r, (function() {
+            var t = this
+              , e = t.$createElement
+              , a = t._self._c || e;
+            return a("transition", {
+                attrs: {
+                    appear: ""
+                },
+                on: {
+                    appear: t.onEnter,
+                    enter: t.onEnter,
+                    leave: t.onLeave
+                }
+            }, [a("div", {
+                staticClass: "FeedLogin",
+                class: {
+                    "FeedLogin--fullScreen": t.$props.isFullscreen
+                },
+                on: {
+                    click: t.dismiss
+                }
+            }, [a("LogInCard", {
+                class: {
+                    FeedLogin__fullscreenCard: t.$props.isFullscreen
+                },
+                attrs: {
+                    "logged-in": t.$props.loggedIn
+                },
+                on: {
+                    "on-welcome": function(e) {
+                        return t.$emit("on-welcome")
+                    }
+                }
+            })], 1)])
+        }
+        ), [], !1, null, "4ae7a467", null);
+        e.default = s.exports
+    },
+    1087: function(t, e, a) {
+        "use strict";
+        a.r(e);
+        var n = a(0)
+          , o = a(2)
+          , i = a(307)
+          , r = a(37)
+          , d = n.default.extend({
+            name: "LoginCard",
+            components: {
+                SignInButton: i.default,
+                RectangleButton: r.default
+            },
+            props: {
+                loggedIn: Boolean
+            },
+            data: function() {
+                return {
+                    showParticleAnimation: !1,
+                    timeRemaining: 1,
+                    buttonActive: !0,
+                    pinFoamFinger: a(1068),
+                    pinGlasses: a(1069),
+                    jessie: a(1070)
+                }
+            },
+            computed: {
+                title: function() {
+                    return this.loggedIn ? "login_reward_title" : "login_explainer_title"
+                }
+            },
+            mounted: function() {},
+            methods: {
+                onSignIn: function() {
+                    this.$accessor.modal.openLoginModal(!0)
+                },
+                onWelcome: function() {
+                    this.$emit("on-welcome")
+                },
+                onEnter: function(t) {},
+                onLeave: function(t) {
+                    o.a.to(t, {
+                        duration: .4,
+                        opacity: 0,
+                        ease: "Power2.easeInOut"
+                    })
+                },
+                updateTimeRemaining: function(t) {
+                    this.timeRemaining = t
+                }
+            }
+        })
+          , s = (a(1071),
+        a(1))
+          , c = Object(s.a)(d, (function() {
+            var t = this
+              , e = t.$createElement
+              , a = t._self._c || e;
+            return a("transition", {
+                attrs: {
+                    appear: ""
+                },
+                on: {
+                    appear: t.onEnter,
+                    enter: t.onEnter,
+                    leave: t.onLeave
+                }
+            }, [a("div", {
+                staticClass: "LoginCard"
+            }, [a("div", {
+                staticClass: "LoginCard__container"
+            }, [a("div", {
+                staticClass: "LoginCard__background"
+            }), t._v(" "), t.$props.loggedIn ? t._e() : a("div", {
+                staticClass: "LoginCard__imageContainer"
+            }, [a("img", {
+                staticClass: "LoginCard__imageContainer__image",
+                attrs: {
+                    src: t.jessie
+                }
+            })]), t._v(" "), a("div", {
+                directives: [{
+                    name: "class-mods",
+                    rawName: "v-class-mods",
+                    value: {
+                        connected: t.$props.loggedIn
+                    },
+                    expression: "{\n          connected: $props.loggedIn,\n        }"
+                }],
+                ref: "loginLabel",
+                staticClass: "LoginCard__text"
+            }, [a("h3", [t._v(t._s(t.$t(t.title)))]), t._v(" "), t.$props.loggedIn ? a("h4", [t._v(t._s(t.$t("login_explainer_desc")))]) : t._e()]), t._v(" "), a("div", {
+                directives: [{
+                    name: "class-mods",
+                    rawName: "v-class-mods",
+                    value: {
+                        connected: t.$props.loggedIn
+                    },
+                    expression: "{\n          connected: $props.loggedIn,\n        }"
+                }],
+                staticClass: "LoginCard__decorationLeft"
+            }, [a("img", {
+                staticClass: "LoginCard__decorationLeft__image",
+                attrs: {
+                    src: t.pinFoamFinger
+                }
+            })]), t._v(" "), a("div", {
+                staticClass: "LoginCard__decorationRight"
+            }, [a("img", {
+                staticClass: "LoginCard__decorationRight__image",
+                attrs: {
+                    src: t.pinGlasses
+                }
+            })])]), t._v(" "), a("div", {
+                ref: "signInButton",
+                staticClass: "LoginCard__buttonContainer"
+            }, [t.$props.loggedIn ? a("RectangleButton", {
+                class: "LoginCard__buttonContainer__button",
+                attrs: {
+                    color: "peach",
+                    stretch: ""
+                },
+                on: {
+                    "on-click": t.onWelcome
+                }
+            }, [t._v("\n        " + t._s(t.$t("login_reward_cta")) + "\n      ")]) : a("SignInButton", {
+                staticClass: "LoginCard__buttonContainer__button",
+                nativeOn: {
+                    click: function(e) {
+                        return t.onSignIn.apply(null, arguments)
+                    }
+                }
+            })], 1)])])
+        }
+        ), [], !1, null, "0bccffe1", null);
+        e.default = c.exports
+    },
+    1090: function(t, e, a) {
+        "use strict";
+        a(1058)
+    },
+    1091: function(t, e, a) {
+        var n = a(7)((function(t) {
+            return t[1]
+        }
+        ));
+        n.push([t.i, ".FeedLogin[data-v-4ae7a467]{position:absolute;width:100%;height:100%;top:0;bottom:0;left:0;right:0;display:flex;align-items:center;justify-content:flex-start;flex-direction:column;padding:15vh 24px 24px;pointer-events:auto;max-width:490px;margin:0 auto}.FeedLogin--fullScreen[data-v-4ae7a467]{justify-content:center;padding-top:24px;background-color:rgba(0,0,0,.5);max-width:unset}.FeedLogin__fullscreenCard[data-v-4ae7a467]{max-width:340px}@media(max-width:23.4275em){.FeedLogin[data-v-4ae7a467]{padding:100px 16px 16px}}", ""]),
+        n.locals = {},
+        t.exports = n
+    },
+    1126: function(t, e, a) {
+        var n = a(1182);
+        n.__esModule && (n = n.default),
+        "string" == typeof n && (n = [[t.i, n, ""]]),
+        n.locals && (t.exports = n.locals);
+        (0,
+        a(8).default)("a62f2322", n, !0, {
+            sourceMap: !1
+        })
+    },
+    1181: function(t, e, a) {
+        "use strict";
+        a(1126)
+    },
+    1182: function(t, e, a) {
+        var n = a(7)((function(t) {
+            return t[1]
+        }
+        ));
+        n.push([t.i, ".rightOffset[data-v-5f666859]{left:-400px}.FullscreenFeed[data-v-5f666859]{position:absolute;width:100%;height:100%;top:0;bottom:0;left:0;right:0}.FullscreenFeed__content[data-v-5f666859]{display:flex;justify-content:center;align-items:center;pointer-events:auto;position:absolute;height:100%;left:50%;transform:translateX(-50%);width:340px}.FullscreenFeed__content>div[data-v-5f666859]{width:100%}", ""]),
+        n.locals = {},
+        t.exports = n
+    },
+    1241: function(t, e, a) {
+        "use strict";
+        a.r(e);
+        var n = a(230)
+          , o = a(312)
+          , i = a(313)
+          , r = a(315)
+          , d = a(314)
+          , s = a(316)
+          , c = a(317)
+          , f = {
+            name: "Feed",
+            components: {
+                MultiChoiceQuestionCard: n.a,
+                SliderQuestionCard: o.default,
+                MatchPredictionQuestionCard: i.default,
+                PostMatchCard: r.default,
+                RewardCard: d.default,
+                LootDropCard: s.default,
+                OnboardingCard: c.default
+            },
+            props: {
+                card: {
+                    type: Object,
+                    default: function() {}
+                },
+                loggedIn: Boolean,
+                loginDismissed: Boolean,
+                hideFeedLogin: Boolean
+            },
+            data: function() {
+                return {}
+            },
+            computed: {
+                isLiveInGameplay: function() {
+                    return this.$accessor.global.isLiveInGameplay
+                },
+                showFeedLogin: function() {
+                    return !this.hideFeedLogin && !this.loginDismissed
+                }
+            },
+            methods: {
+                dismissCard: function(t) {
+                    this.$accessor.feed.dismissCard(t)
+                },
+                dismissLogin: function() {
+                    this.$emit("on-dismiss-overlay")
+                }
+            }
+        }
+          , l = (a(1181),
+        a(1))
+          , g = Object(l.a)(f, (function() {
+            var t = this
+              , e = t.$createElement
+              , a = t._self._c || e;
+            return a("div", {
+                staticClass: "FullscreenFeed"
+            }, [t.showFeedLogin ? a("FeedLogin", {
+                attrs: {
+                    "is-fullscreen": "",
+                    "logged-in": t.$props.loggedIn
+                },
+                on: {
+                    "on-dismiss-login": t.dismissLogin,
+                    "on-welcome": function(e) {
+                        return t.$emit("on-welcome")
+                    }
+                }
+            }) : t._e(), t._v(" "), t.card ? a("div", {
+                staticClass: "FullscreenFeed__content"
+            }, [a(t.card.name, t._b({
+                key: t.card.key,
+                tag: "component",
+                attrs: {
+                    "is-fullscreen": "",
+                    active: ""
+                },
+                on: {
+                    "close-card": function() {
+                        return t.dismissCard(t.card)
+                    }
+                }
+            }, "component", t.card.props, !1))], 1) : t._e()], 1)
+        }
+        ), [], !1, null, "5f666859", null);
+        e.default = g.exports;
+        installComponents(g, {
+            FeedLogin: a(1074).default
+        })
+    }
+}]);
