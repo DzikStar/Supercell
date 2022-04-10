9269e53a4924bd3b560096c28a18e0bb59505524
DzikStar <82441193+DzikStar@users.noreply.github.com>
2022-04-10 20:10:58 +0200

    2022-04-10

---
+++ Pages/event.brawlstars.com/_nuxt/9747e0d.js
@@ -0,0 +1,47 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([[11], {
+    1140: function(t, a, e) {
+        var n = e(1199);
+        n.__esModule && (n = n.default),
+        "string" == typeof n && (n = [[t.i, n, ""]]),
+        n.locals && (t.exports = n.locals);
+        (0,
+        e(8).default)("7991ade8", n, !0, {
+            sourceMap: !1
+        })
+    },
+    1198: function(t, a, e) {
+        "use strict";
+        e(1140)
+    },
+    1199: function(t, a, e) {
+        var n = e(7)((function(t) {
+            return t[1]
+        }
+        ));
+        n.push([t.i, ".StaticCard[data-v-18557d4e]{display:flex;flex-direction:column;width:100%}", ""]),
+        n.locals = {},
+        t.exports = n
+    },
+    1245: function(t, a, e) {
+        "use strict";
+        e.r(a);
+        var n = {
+            name: "StaticCard",
+            components: {
+                BaseCard: e(325).default
+            }
+        }
+          , s = (e(1198),
+        e(1))
+          , c = Object(s.a)(n, (function() {
+            var t = this
+              , a = t.$createElement
+              , e = t._self._c || a;
+            return e("BaseCard", [e("div", {
+                staticClass: "StaticCard"
+            }, [t._t("default")], 2)])
+        }
+        ), [], !1, null, "18557d4e", null);
+        a.default = c.exports
+    }
+}]);
