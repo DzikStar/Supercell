fc5b8bc248eb6b42fd6f71a71932291c3c676f3e
DzikStar <82441193+DzikStar@users.noreply.github.com>
2022-04-10 20:35:18 +0200

    2022-04-10

---
+++ Pages/event.brawlstars.com/_nuxt/25971bf.js
@@ -0,0 +1,175 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([[24], {
+    1049: function(a, t, n) {
+        var e = n(1072);
+        e.__esModule && (e = e.default),
+        "string" == typeof e && (e = [[a.i, e, ""]]),
+        e.locals && (a.exports = e.locals);
+        (0,
+        n(8).default)("22ad4a68", e, !0, {
+            sourceMap: !1
+        })
+    },
+    1068: function(a, t, n) {
+        a.exports = n.p + "img/pin_emo_foam_blue.d0a4293.svg"
+    },
+    1069: function(a, t, n) {
+        a.exports = n.p + "img/pin_emo_glasses.74fa12a.svg"
+    },
+    1070: function(a, t, n) {
+        a.exports = n.p + "img/jessie-crop.45c3e88.png"
+    },
+    1071: function(a, t, n) {
+        "use strict";
+        n(1049)
+    },
+    1072: function(a, t, n) {
+        var e = n(7)
+          , o = n(100)
+          , i = n(310)
+          , r = e((function(a) {
+            return a[1]
+        }
+        ))
+          , c = o(i);
+        r.push([a.i, '.LoginCard[data-v-0bccffe1]{box-shadow:0 2px #040404;border-radius:5px;border:2px solid #040404;justify-content:center;flex-direction:column}.LoginCard[data-v-0bccffe1],.LoginCard__container[data-v-0bccffe1]{position:relative;display:flex;align-items:center;width:100%}.LoginCard__container[data-v-0bccffe1]{flex-direction:row}.LoginCard__background[data-v-0bccffe1],.LoginCard__container[data-v-0bccffe1]{border-top-left-radius:4px;border-top-right-radius:4px;min-height:145px}.LoginCard__background[data-v-0bccffe1]{position:absolute;background-color:#ffd550;overflow:hidden;height:100%;width:100%}.LoginCard__background[data-v-0bccffe1]:after{position:absolute;width:100%;height:100%;top:0;bottom:0;left:0;right:0;content:" ";padding:4px;-webkit-mask-repeat:no-repeat;mask-repeat:no-repeat;-webkit-mask-size:cover;mask-size:cover;-webkit-mask-position:center center;mask-position:center center;background-color:#ffc938;-webkit-mask-image:url(' + c + ");mask-image:url(" + c + ')}.LoginCard h3[data-v-0bccffe1]{text-shadow:-1.5px -1.5px 0 #040404,0 -1.5px 0 #040404,1.5px -1.5px 0 #040404,1.5px 0 0 #040404,1.5px 1.5px 0 #040404,0 1.5px 0 #040404,-1.5px 1.5px 0 #040404,-1.5px 0 0 #040404,0 3px 0 #040404,-1.5px 3px 0 #040404,1.5px 3px 0 #040404;color:#fff;font-weight:400;font-family:"Lilita One",sans-serif;text-transform:none;font-size:18px}.LoginCard h3[data-v-0bccffe1]:lang(jp),.LoginCard h3[data-v-0bccffe1]:lang(kr),.LoginCard h3[data-v-0bccffe1]:lang(pl),.LoginCard h3[data-v-0bccffe1]:lang(ru),.LoginCard h3[data-v-0bccffe1]:lang(tr),.LoginCard h3[data-v-0bccffe1]:lang(zh_sc),.LoginCard h3[data-v-0bccffe1]:lang(zh_tc){font-family:"NotoSans",sans-serif;font-weight:700}.LoginCard h4[data-v-0bccffe1]{color:#040404;font-size:14px;font-family:"Montserrat",sans-serif;text-align:center;margin-top:8px;color:#000}.LoginCard h4[data-v-0bccffe1]:lang(jp),.LoginCard h4[data-v-0bccffe1]:lang(kr),.LoginCard h4[data-v-0bccffe1]:lang(pl),.LoginCard h4[data-v-0bccffe1]:lang(ru),.LoginCard h4[data-v-0bccffe1]:lang(tr),.LoginCard h4[data-v-0bccffe1]:lang(zh_sc),.LoginCard h4[data-v-0bccffe1]:lang(zh_tc){font-family:"NotoSans",sans-serif;font-weight:700}.LoginCard__decorationLeft[data-v-0bccffe1]{position:absolute;top:-28px;left:30%;width:80px;height:60px;transform:rotate(-25deg)}.LoginCard__decorationLeft__image[data-v-0bccffe1]{width:100%;height:auto}.LoginCard__decorationLeft--connected[data-v-0bccffe1]{top:-16px;left:8px;transform:rotate(-55deg)}.LoginCard__decorationRight[data-v-0bccffe1]{position:absolute;width:80px;height:60px;top:-15px;right:-15px;transform:rotate(25deg)}.LoginCard__decorationRight__image[data-v-0bccffe1]{width:100%;height:auto}.LoginCard__text[data-v-0bccffe1]{position:relative;width:60%;padding:32px 16px 16px}.LoginCard__text--connected[data-v-0bccffe1]{display:flex;justify-content:center;align-items:center;flex-direction:column;width:100%;text-align:center;padding:16px}.LoginCard__imageContainer[data-v-0bccffe1]{position:relative;display:flex;width:33%;height:145px}.LoginCard__imageContainer__image[data-v-0bccffe1]{position:absolute;bottom:0;left:0;height:160px;width:auto}.LoginCard__buttonContainer[data-v-0bccffe1]{position:relative;height:64px;width:100%;display:flex;justify-content:center;align-items:center;padding:0 12px;background-color:#9d4540;border-bottom-left-radius:4px;border-bottom-right-radius:4px;border-top:2px solid #040404}.LoginCard__buttonContainer__button[data-v-0bccffe1]{min-width:100%}@media(max-width:23.4275em){.LoginCard h3[data-v-0bccffe1]{text-shadow:-1.5px -1.5px 0 #040404,0 -1.5px 0 #040404,1.5px -1.5px 0 #040404,1.5px 0 0 #040404,1.5px 1.5px 0 #040404,0 1.5px 0 #040404,-1.5px 1.5px 0 #040404,-1.5px 0 0 #040404,0 3px 0 #040404,-1.5px 3px 0 #040404,1.5px 3px 0 #040404;color:#fff;font-weight:400;font-family:"Lilita One",sans-serif;text-transform:none;font-size:18px}.LoginCard h3[data-v-0bccffe1]:lang(jp),.LoginCard h3[data-v-0bccffe1]:lang(kr),.LoginCard h3[data-v-0bccffe1]:lang(pl),.LoginCard h3[data-v-0bccffe1]:lang(ru),.LoginCard h3[data-v-0bccffe1]:lang(tr),.LoginCard h3[data-v-0bccffe1]:lang(zh_sc),.LoginCard h3[data-v-0bccffe1]:lang(zh_tc){font-family:"NotoSans",sans-serif;font-weight:700}.LoginCard__container[data-v-0bccffe1]{min-height:115px}.LoginCard__imageContainer[data-v-0bccffe1]{height:115px}.LoginCard__imageContainer__image[data-v-0bccffe1]{height:145px}.LoginCard__decorationLeft[data-v-0bccffe1]{transform:translate(15px,-10px) scale(.9) rotate(-35deg)}.LoginCard__decorationRight[data-v-0bccffe1]{transform:translate(10px,-12px) scale(.9) rotate(25deg)}}@media only screen and (max-device-width:375px)and (orientation:landscape){.LoginCard h3[data-v-0bccffe1]{text-shadow:-1.5px -1.5px 0 #040404,0 -1.5px 0 #040404,1.5px -1.5px 0 #040404,1.5px 0 0 #040404,1.5px 1.5px 0 #040404,0 1.5px 0 #040404,-1.5px 1.5px 0 #040404,-1.5px 0 0 #040404,0 3px 0 #040404,-1.5px 3px 0 #040404,1.5px 3px 0 #040404;color:#fff;font-weight:400;font-family:"Lilita One",sans-serif;text-transform:none;font-size:18px}.LoginCard h3[data-v-0bccffe1]:lang(jp),.LoginCard h3[data-v-0bccffe1]:lang(kr),.LoginCard h3[data-v-0bccffe1]:lang(pl),.LoginCard h3[data-v-0bccffe1]:lang(ru),.LoginCard h3[data-v-0bccffe1]:lang(tr),.LoginCard h3[data-v-0bccffe1]:lang(zh_sc),.LoginCard h3[data-v-0bccffe1]:lang(zh_tc){font-family:"NotoSans",sans-serif;font-weight:700}.LoginCard__container[data-v-0bccffe1]{min-height:115px}.LoginCard__imageContainer[data-v-0bccffe1]{height:115px}.LoginCard__imageContainer__image[data-v-0bccffe1]{height:145px}.LoginCard__decorationLeft[data-v-0bccffe1]{transform:translate(15px,-10px) scale(.9) rotate(-35deg)}.LoginCard__decorationRight[data-v-0bccffe1]{transform:translate(10px,-12px) scale(.9) rotate(25deg)}}', ""]),
+        r.locals = {},
+        a.exports = r
+    },
+    1087: function(a, t, n) {
+        "use strict";
+        n.r(t);
+        var e = n(0)
+          , o = n(2)
+          , i = n(307)
+          , r = n(37)
+          , c = e.default.extend({
+            name: "LoginCard",
+            components: {
+                SignInButton: i.default,
+                RectangleButton: r.default
+            },
+            props: {
+                loggedIn: Boolean
+            },
+            data: function() {
+                return {
+                    showParticleAnimation: !1,
+                    timeRemaining: 1,
+                    buttonActive: !0,
+                    pinFoamFinger: n(1068),
+                    pinGlasses: n(1069),
+                    jessie: n(1070)
+                }
+            },
+            computed: {
+                title: function() {
+                    return this.loggedIn ? "login_reward_title" : "login_explainer_title"
+                }
+            },
+            mounted: function() {},
+            methods: {
+                onSignIn: function() {
+                    this.$accessor.modal.openLoginModal(!0)
+                },
+                onWelcome: function() {
+                    this.$emit("on-welcome")
+                },
+                onEnter: function(a) {},
+                onLeave: function(a) {
+                    o.a.to(a, {
+                        duration: .4,
+                        opacity: 0,
+                        ease: "Power2.easeInOut"
+                    })
+                },
+                updateTimeRemaining: function(a) {
+                    this.timeRemaining = a
+                }
+            }
+        })
+          , d = (n(1071),
+        n(1))
+          , f = Object(d.a)(c, (function() {
+            var a = this
+              , t = a.$createElement
+              , n = a._self._c || t;
+            return n("transition", {
+                attrs: {
+                    appear: ""
+                },
+                on: {
+                    appear: a.onEnter,
+                    enter: a.onEnter,
+                    leave: a.onLeave
+                }
+            }, [n("div", {
+                staticClass: "LoginCard"
+            }, [n("div", {
+                staticClass: "LoginCard__container"
+            }, [n("div", {
+                staticClass: "LoginCard__background"
+            }), a._v(" "), a.$props.loggedIn ? a._e() : n("div", {
+                staticClass: "LoginCard__imageContainer"
+            }, [n("img", {
+                staticClass: "LoginCard__imageContainer__image",
+                attrs: {
+                    src: a.jessie
+                }
+            })]), a._v(" "), n("div", {
+                directives: [{
+                    name: "class-mods",
+                    rawName: "v-class-mods",
+                    value: {
+                        connected: a.$props.loggedIn
+                    },
+                    expression: "{\n          connected: $props.loggedIn,\n        }"
+                }],
+                ref: "loginLabel",
+                staticClass: "LoginCard__text"
+            }, [n("h3", [a._v(a._s(a.$t(a.title)))]), a._v(" "), a.$props.loggedIn ? n("h4", [a._v(a._s(a.$t("login_explainer_desc")))]) : a._e()]), a._v(" "), n("div", {
+                directives: [{
+                    name: "class-mods",
+                    rawName: "v-class-mods",
+                    value: {
+                        connected: a.$props.loggedIn
+                    },
+                    expression: "{\n          connected: $props.loggedIn,\n        }"
+                }],
+                staticClass: "LoginCard__decorationLeft"
+            }, [n("img", {
+                staticClass: "LoginCard__decorationLeft__image",
+                attrs: {
+                    src: a.pinFoamFinger
+                }
+            })]), a._v(" "), n("div", {
+                staticClass: "LoginCard__decorationRight"
+            }, [n("img", {
+                staticClass: "LoginCard__decorationRight__image",
+                attrs: {
+                    src: a.pinGlasses
+                }
+            })])]), a._v(" "), n("div", {
+                ref: "signInButton",
+                staticClass: "LoginCard__buttonContainer"
+            }, [a.$props.loggedIn ? n("RectangleButton", {
+                class: "LoginCard__buttonContainer__button",
+                attrs: {
+                    color: "peach",
+                    stretch: ""
+                },
+                on: {
+                    "on-click": a.onWelcome
+                }
+            }, [a._v("\n        " + a._s(a.$t("login_reward_cta")) + "\n      ")]) : n("SignInButton", {
+                staticClass: "LoginCard__buttonContainer__button",
+                nativeOn: {
+                    click: function(t) {
+                        return a.onSignIn.apply(null, arguments)
+                    }
+                }
+            })], 1)])])
+        }
+        ), [], !1, null, "0bccffe1", null);
+        t.default = f.exports
+    }
+}]);
